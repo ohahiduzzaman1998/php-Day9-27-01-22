06eefee3af8258c48c3c3d23ccb38c71f38b4943
ohahiduzzaman1998 <ohahiduzzaman1998@gmail.com>
2022-01-27 13:13:30 +0600

    php with xampp

---
+++ app/classes/HelloWorld.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\classes;
+//namespace auto load
+
+
+class HelloWorld
+{
+
+    public $name = 'Bitm';
+    public function index()
+    {
+        echo "Bangladesh is my country",$this->name;
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+
+require_once 'vendor/autoload.php';
+
+//requ on ekta file link kora jay
+use app\classes\HelloWorld;
+
+$helloWorld = new HelloWorld();
+$helloWorld->index();
